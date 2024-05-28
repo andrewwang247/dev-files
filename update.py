0c1ef99d6416c03a407a21a875676cae1e4443b6
Andrew Wang <70552426+andrewwang247@users.noreply.github.com>
2024-05-28 13:08:46 -0700

    Allow option to update package individually

--- update.py
+++ update.py
@@ -1,23 +1,27 @@
 """
 Update all Python packages.
 
-Copyright 2021. Andrew Wang.
+Copyright 2024. Andrew Wang.
 """
-from json import loads
-from subprocess import run, PIPE
-from platform import python_version
+# pylint: disable=not-an-iterable, no-value-for-parameter
+from concurrent.futures import ThreadPoolExecutor
+from subprocess import run
+from click import command, option
+from pkg_resources import working_set
 
 
-def update():
-    """Pipe output from pip list and update all."""
-    version = python_version()[0]
-    proc = run(f'python{version} -m pip list --format json'.split(),
-               stdout=PIPE, check=True)
-    output = loads(proc.stdout)
-    packages = [entry['name'] for entry in output]
-    command = f'python{version} -m pip install -U'.split() + packages
-    print(f'PYTHON {version} - {len(packages)} PACKAGES')
-    run(command, check=True)
+@command()
+@option('--individual', '-i', is_flag=True, show_default=True,
+        default=False, help='Update packages together / individually')
+def update(individual: bool):
+    """Run pip update command."""
+    pkgs = [dist.project_name for dist in working_set]
+    if individual:
+        with ThreadPoolExecutor() as pool:
+            pool.map(lambda pkg: run(
+                ['pip', 'install', '-U', pkg], check=True), pkgs)
+    else:
+        run(['pip', 'install', '-U'] + pkgs, check=True)
 
 
 if __name__ == '__main__':
